91302b9f5250e2833af49a1ddec9da42b6940966
Adrian Moreno <admoreno@outlook.com>
2016-05-18 20:39:05 +0200

    Fix angular-chartjs
    Fix angular-chartjs by importing the correct version of Charts.js,
    since angular-chartjs doesn't support v2.x of Charts.js. The version
    used is v1.1.1

--- public/scripts/controllers/stats-controller.js
+++ public/scripts/controllers/stats-controller.js
@@ -1,12 +1,13 @@
-angular.module('PoisRUs').controller('StatsCtrl', function($scope) {
-        $scope.prueba = "prueba";
-        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+angular.module('PoisRUs').controller('StatsCtrl', [
+    '$scope',
+    function($scope) {
+        $scope.labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
         $scope.series = ['Series A', 'Series B'];
         $scope.data = [
             [65, 59, 80, 81, 56, 55, 40],
             [28, 48, 40, 19, 86, 27, 90]
         ];
-        $scope.onClick = function (points, evt) {
+        $scope.onChartClick = function (points, evt) {
             console.log(points, evt);
         };
-    });
+    }]);
