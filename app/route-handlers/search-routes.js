7284acb7df66f5cac8e4af91f3da3897217e8e23
Eduardo Ibáñez <eduiba94@gmail.com>
2016-05-19 20:16:41 +0200

    changes in search API

--- app/route-handlers/search-routes.js
+++ app/route-handlers/search-routes.js
@@ -3,22 +3,22 @@ var apiPaths = require('../../common/api-routes');
 var UserModel = require('../models/user-model');
 var PoiModel = require('../models/poi-model');
 var formatConversor = require('../format-conversor');
-var authMiddleware  = require('./auth-middleware');
 
 var router = express.Router();
 
 router.route(apiPaths.SEARCH_USERS)
     .get(function(req, res) {
-        UserModel.find({$or:[
-            {"first_name" : "/.*" + req.query.first_name + ".*/"},
-            {"last_name" : "/.*" + req.query.last_name + ".*/"}
-        ]}, function(err, data) {
+        UserModel.find({
+            $or: [
+                {first_name: {'$regex': req.query.first_name}},
+                {last_name: {'$regex': req.query.last_name}}
+            ]
+        }, function (err, data) {
             var response = {};
-            if(err) {
-                response = { error: true, message: err };
+            if (err) {
+                response = {error: true, message: err};
                 res.status(500).json(response);
             } else {
-                console.log(req.query.first_name);
                 response = {
                     error: false,
                     message: data.map(formatConversor.userDBtoAPI)
@@ -31,15 +31,14 @@ router.route(apiPaths.SEARCH_USERS)
 router.route(apiPaths.SEARCH_POIS)
     .get(function(req, res) {
         PoiModel.find({$or:[
-            {"name" : "/.*" + req.query.name + ".*/"},
-            {"description" : "/.*" + req.query.description + ".*/"}
+            {name : {'$regex' : req.query.name}},
+            {description : {'$regex' : req.query.description}}
         ]}, function(err, data) {
             var response = {};
             if(err) {
                 response = { error: true, message: err };
                 res.status(500).json(response);
             } else {
-                console.log(req.query.name);
                 response = {
                     error: false,
                     message: data.map(formatConversor.poiDBtoAPI)
