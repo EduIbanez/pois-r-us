bc64299397f94b0e2c01626a7765dbe2f89dc216
dforcen <dforcen@hotmail.com>
2016-05-19 20:20:19 +0200

    busquedas

--- app/route-handlers/search-routes.js
+++ app/route-handlers/search-routes.js
@@ -3,20 +3,19 @@ var apiPaths = require('../../common/api-routes');
 var UserModel = require('../models/user-model');
 var PoiModel = require('../models/poi-model');
 var formatConversor = require('../format-conversor');
+var authMiddleware  = require('./auth-middleware');
 
 var router = express.Router();
 
 router.route(apiPaths.SEARCH_USERS)
     .get(function(req, res) {
-        UserModel.find({
-            $or: [
-                {first_name: {'$regex': req.query.first_name}},
-                {last_name: {'$regex': req.query.last_name}}
-            ]
-        }, function (err, data) {
+        UserModel.find({$or:[
+            {"first_name" : {'$regex' : req.query.first_name}},
+            {"last_name" : {'$regex' : req.query.first_name}},
+        ]}, function(err, data) {
             var response = {};
-            if (err) {
-                response = {error: true, message: err};
+            if(err) {
+                response = { error: true, message: err };
                 res.status(500).json(response);
             } else {
                 response = {
@@ -31,14 +30,15 @@ router.route(apiPaths.SEARCH_USERS)
 router.route(apiPaths.SEARCH_POIS)
     .get(function(req, res) {
         PoiModel.find({$or:[
-            {name : {'$regex' : req.query.name}},
-            {description : {'$regex' : req.query.description}}
+            {"name" : {'$regex' : req.query.name }},
+            {"description" : {'$regex' : req.query.description }},
         ]}, function(err, data) {
             var response = {};
             if(err) {
                 response = { error: true, message: err };
                 res.status(500).json(response);
             } else {
+                console.log(req.query.name);
                 response = {
                     error: false,
                     message: data.map(formatConversor.poiDBtoAPI)
