837db543c15f28604ed33635d98d032bf3a751de
Eduardo Ibáñez <eduiba94@gmail.com>
2016-05-19 20:24:44 +0200

    changes in search API

--- app/route-handlers/search-routes.js
+++ app/route-handlers/search-routes.js
@@ -10,8 +10,8 @@ var router = express.Router();
 router.route(apiPaths.SEARCH_USERS)
     .get(function(req, res) {
         UserModel.find({$or:[
-            {"first_name" : {'$regex' : req.query.first_name}},
-            {"last_name" : {'$regex' : req.query.first_name}},
+            {"first_name" : {'$regex' : req.query.data}},
+            {"last_name" : {'$regex' : req.query.data}}
         ]}, function(err, data) {
             var response = {};
             if(err) {
@@ -30,15 +30,14 @@ router.route(apiPaths.SEARCH_USERS)
 router.route(apiPaths.SEARCH_POIS)
     .get(function(req, res) {
         PoiModel.find({$or:[
-            {"name" : {'$regex' : req.query.name }},
-            {"description" : {'$regex' : req.query.description }},
+            {"name" : {'$regex' : req.query.data}},
+            {"description" : {'$regex' : req.query.data }}
         ]}, function(err, data) {
             var response = {};
             if(err) {
                 response = { error: true, message: err };
                 res.status(500).json(response);
             } else {
-                console.log(req.query.name);
                 response = {
                     error: false,
                     message: data.map(formatConversor.poiDBtoAPI)
